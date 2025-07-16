21ff9c4dffba313e8ea2b9b29a4b3b87eec5c798
Pharnuwat <se56160411@gmail.com>
2025-07-16 14:31:41 +0700

    edit: update logical condition in ex-1.js

--- ex-1.js
+++ ex-1.js
@@ -6,5 +6,5 @@ let isAllow;
 // Start coding here
 isOver18 = false;
 hasCriminalBlacklist = false;
-isAllow = isOver18 && hasCriminalBlacklist;
+isAllow = isOver18 && !hasCriminalBlacklist;
 console.log(isAllow);
